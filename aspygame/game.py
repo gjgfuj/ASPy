0422db32141d82cfbe436cba52031c8417bf518c
gjgfuj <gjgfujb@gmail.com>
2014-05-28 22:32:16 +1000

    Started game.

--- aspygame/game.py
+++ aspygame/game.py
@@ -1,5 +1,13 @@
 import pygame
 pygame.init()
 import aspy.games
-def setup():
+import aspy.game
+class InitLoader(aspy.games.Loader):
+  def loadmenu(self):
+    return "Menu Loaded"
+  def __init__(self, menu):
+    aspy.games.Loader.__init__(self, [], menu)
+class MainMenu(aspy.games.Menu):
   pass
+def setup():
+  aspy.game.game = aspy.games.Load(InitLoader(MainMenu()))
