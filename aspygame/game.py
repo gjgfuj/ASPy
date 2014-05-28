f47eba000d3423060a096caab809a7e836f15739
gjgfuj <gjgfujb@gmail.com>
2014-05-28 23:29:09 +1000

    Updated game.py

--- aspygame/game.py
+++ aspygame/game.py
@@ -5,12 +5,12 @@ import aspy.loader
 import aspy.game
 class InitLoader(aspy.loader.Loader):
   def loadmenu(self):
-    return "Menu Loaded"
+    pass
   def __init__(self, menu):
     aspy.loader.Loader.__init__(self, [], menu)
 class MainMenu(aspy.games.Menu):
   def startgame(self):
-    pass
+    print "Game Started"
   def __init__(self):
     aspy.games.Menu.__init__(self)
     self.addmenuitem("Start Game", self.startgame)
